c27d1bab5bd72255eaedf5f7ffae1d04f44c7133
TheRyanChang <32142888+TheRyanChang@users.noreply.github.com>
2017-09-25 13:24:17 -0700

    Update PriceListItem.cpp

--- PriceListItem.cpp
+++ PriceListItem.cpp
@@ -2,10 +2,16 @@
 
 PriceListItem::PriceListItem(const string &itemName, const string &code, double price, bool taxable) {
 	// TO BE COMPLETED
+	//initialization parameters
+	ItemName = itemName;
+	ItemCode = code;
+	ItemPrice = price;
+	ItemTax = taxable;
 }
 
 PriceListItem::PriceListItem() {
 	// TO BE COMPLETED
+	//default constructor
 	ItemName = "Apples";
 	ItemCode = 1000;
 	ItemPrice = 1.29;
@@ -15,17 +21,21 @@ PriceListItem::PriceListItem() {
 
 string PriceListItem::getItemName() {
 	// TO BE COMPLETED
+	return ItemName;
 }
 
 string PriceListItem::getCode() {
 	// TO BE COMPLETED
+	return ItemCode;
 }
 
 double PriceListItem::getPrice() {
 	// TO BE COMPLETED
+	return ItemPrice;
 }
 
 bool PriceListItem::isTaxable() {
 	// TO BE COMPLETED
+	return ItemTax;
 }
 
